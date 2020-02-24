aeffb8ca39f2815f8638f7a03fee4bb5773c093c
Felix Radensky <felix.r@variscite.com>
2020-02-24 13:28:42 +0200

    mx8qx_var_som: Add EEPROM support

---
+++ src/scfw_export_mx8qx_b0/platform/board/mx8qx_var_som/eeprom.h
@@ -0,0 +1,17 @@
+#ifndef DRV_EEPROM_H
+#define DRV_EEPROM_H
+
+#define EEPROM_I2C_ADDRESS	0x52
+
+#define CTL_CODE(function, method) ((4 << 16) | ((function) << 2) | (method))
+
+#define METHOD_BUFFERED		0
+#define METHOD_NEITHER		3
+
+#define SOMINFO_READ_EEPROM	CTL_CODE(2100, METHOD_BUFFERED)
+#define SOMINFO_WRITE_EEPROM	CTL_CODE(2101, METHOD_BUFFERED)
+
+extern status_t eeprom_i2c_write(uint8_t device_addr, uint8_t reg, const void *data, uint32_t dataLength);
+extern status_t eeprom_i2c_read(uint8_t device_addr, uint8_t reg, void *data, uint32_t dataLength);
+
+#endif
