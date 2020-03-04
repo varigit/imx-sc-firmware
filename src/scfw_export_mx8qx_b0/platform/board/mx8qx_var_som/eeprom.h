6cb6e1598007c9bcbdd3cb874cdd817d5183f501
Felix Radensky <felix.r@variscite.com>
2020-03-04 15:36:51 +0200

    mx8qx_var_som: Add routine to initialize EEPROM I2C bus
    Reguired to explicitly initialize EEPROM I2C bus in order
    to access EEPROM at early boot stage.

--- src/scfw_export_mx8qx_b0/platform/board/mx8qx_var_som/eeprom.h
+++ src/scfw_export_mx8qx_b0/platform/board/mx8qx_var_som/eeprom.h
@@ -11,6 +11,7 @@
 #define SOMINFO_READ_EEPROM	CTL_CODE(2100, METHOD_BUFFERED)
 #define SOMINFO_WRITE_EEPROM	CTL_CODE(2101, METHOD_BUFFERED)
 
+extern void eeprom_i2c_init(void);
 extern status_t eeprom_i2c_write(uint8_t device_addr, uint8_t reg, const void *data, uint32_t dataLength);
 extern status_t eeprom_i2c_read(uint8_t device_addr, uint8_t reg, void *data, uint32_t dataLength);
 
