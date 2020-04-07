9f1499fb00550bfc8befff4c641d8e6e33f63413
Felix Radensky <felix.r@variscite.com>
2020-04-07 21:33:34 +0300

    mx8qx_b0: Add NXP SC Firmware source code for i.MX8QXP B0 and C0

---
+++ src/scfw_export_mx8qx_b0/platform/main/build_info.h
@@ -0,0 +1,11 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define SCFW_BRANCH imx_4.14.98_2.3.0
+#define SCFW_BUILD 4124UL
+#define SCFW_COMMIT 0xb929edfeUL
+#define SCFW_DATE "Jan 31 2020"
+#define SCFW_DATE2 Jan_31_2020
+#define SCFW_TIME "20:57:35"
+
+#endif
