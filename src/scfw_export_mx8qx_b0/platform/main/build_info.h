0ce72dd48ff879f1bcc3556377086f475af1af1d
mohit <mohit.p@variscite.com>
2021-06-29 12:09:23 -0700

    mx8qx_b0: Add NXP SC Firmware 1.6.0 source code for i.MX8QXP B0 and C0

---
+++ src/scfw_export_mx8qx_b0/platform/main/build_info.h
@@ -0,0 +1,11 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define SCFW_BRANCH imx_scfw_2020q3
+#define SCFW_BUILD 4894UL
+#define SCFW_COMMIT 0x56682d58UL
+#define SCFW_DATE "Sep 25 2020"
+#define SCFW_DATE2 Sep_25_2020
+#define SCFW_TIME "22:42:45"
+
+#endif
