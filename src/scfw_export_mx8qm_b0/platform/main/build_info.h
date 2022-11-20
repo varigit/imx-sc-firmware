f9772b2d7c2fb3d0a5c1b67f6e9ca909475c2207
Diego Dorta <diego.d@variscite.com>
2022-11-20 16:22:50 -0300

    mx8qm_b0: Add NXP SC Firmware 1.13.0 source code for i.MX8QM B0
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ src/scfw_export_mx8qm_b0/platform/main/build_info.h
@@ -0,0 +1,11 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define SCFW_BRANCH imx_scfw_2022q2
+#define SCFW_BUILD 5561UL
+#define SCFW_COMMIT 0x8172eaeaUL
+#define SCFW_DATE "Jun 02 2022"
+#define SCFW_DATE2 Jun_02_2022
+#define SCFW_TIME "02:37:21"
+
+#endif
